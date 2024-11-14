c1faa20d62cc9906a66dd7986f8725bd6481290f
AbubakarMian <abubakrmianmamoon@gmail.com>
2024-11-14 20:28:30 +0500

    data extration basic complete

--- demand_base.js
+++ demand_base.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { chromium } = require('playwright');
+const cors = require('cors');
 const app = express();
 const PORT = 5003;
+app.use(cors());
 
 async function getDataFromPage(url, limit) {
+
+    console.log('url',url);
     const browser = await chromium.launch({
         headless: false,
         args: [
@@ -20,7 +24,7 @@ async function getDataFromPage(url, limit) {
     await page.setExtraHTTPHeaders({
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     });
-    await page.goto(url);
+    await page.goto('https://authentication.demandbase.com/signin');
 
     const usernameSelector = '#okta-signin-username';
     const passwordSelector = '#okta-signin-password';
@@ -44,7 +48,7 @@ async function getDataFromPage(url, limit) {
 
     // page.click(submitDemandbaseOne)
     await page.waitForTimeout(10000);
-    await page.goto('https://web.demandbase.com/sales/company/1026419/people'); //1026419,727617
+    await page.goto(url); //1026419,727617
 
     const tableXPath = '//*[@id="content"]/div[2]/div/div[3]/div/div/div/div[2]/div[2]/div/div[2]/div/div[4]/div[1]/div/div';
     let retries = 100;
@@ -76,18 +80,23 @@ async function getDataFromPage(url, limit) {
 
     const data = [];
     let total_indexed = 0;
-    let r_num = 0;
     const uniqueRecords = new Set();
     let continue_search = true;
     let uniq_not_found = 0;
-    while (total_indexed < totalNumber&& continue_search) {
+    let iterated_data = 0;
+    while (total_indexed < totalNumber&& continue_search) {//&&iterated_data<totalNumber
+        iterated_data++;
+        rows = await page.$$('.people-lists__list');
+        await page.waitForTimeout(3000);
+        total_rows = rows.length;
+        console.log('part rows : ', total_rows);
 
-        r_num = 0;
         for (let index = 0; index < total_rows; index++) {
-            const nameElement = await rows[index].$('.people-lists__list__contacts__details__name');
+            
             try {
-                let check_name = await nameElement.innerText();
-                console.log('check_name:', check_name);
+                // const nameElement = await rows[index].$('.people-lists__list__contacts__details__name');
+                // let check_name = await nameElement.innerText();
+                // console.log('check_name:', check_name);
                 let click_index_need = true;
                 for (let click_index = 0; click_index < 3 && click_index_need; click_index++) {
                     try {
@@ -100,34 +109,51 @@ async function getDataFromPage(url, limit) {
                     } catch (error) {
                         console.log('cannpot click row index trying again :', index);
                         await page.waitForTimeout(1500);
-
                     }
                 }
 
-                await page.waitForTimeout(1500);
+                let loaddata = true;
+                for (let loaddata__index = 0; loaddata__index < 7 && loaddata; loaddata__index++) {
+                    try {
 
-                const name = (await (await page.$('.exec-firmo-compact__executive-name-text'))?.innerText()) || "";
-                const title = (await (await page.$('.exec-firmo-compact__summary__executive__title'))?.innerText()) || "";
-                const email = (await (await page.$('.exec-firmo-compact__people-email-value'))?.innerText()) || "";
-                const phone = (await (await page.$('.exec-firmo-compact__people-phone'))?.innerText()) || "";
-                const home = (await (await page.$('.expand.direct'))?.innerText()) || "";
-                console.log(`Information: ${name}, ${title}, ${email}, ${phone}, ${home} Rown Number ${total_indexed}`);
+                        const name = (await (await page.$('.exec-firmo-compact__executive-name-text'))?.innerText()) || "";
+                        
+                        if(name==""){
+                            await page.waitForTimeout(300);
+                            continue;
+                        }
+                        loaddata = false;
+                        await page.waitForTimeout(500);
+                        const title = (await (await page.$('.exec-firmo-compact__summary__executive__title'))?.innerText()) || "";
+                        const email = (await (await page.$('.exec-firmo-compact__people-email-value'))?.innerText()) || "";
+                        const phone = (await (await page.$('.exec-firmo-compact__people-phone'))?.innerText()) || "";
+                        const home = (await (await page.$('.expand.direct'))?.innerText()) || "";
+                        console.log(`Information: ${name}, ${title}, ${email}, ${phone}, ${home} Rown Number ${total_indexed}`);
+                        console.log('iterated_data :', iterated_data);
+        
+                        const uniqueKey = `${name}-${title}-${email}-${phone}-${home}`;
+                        
+                        if (!uniqueRecords.has(uniqueKey)) {
+                            uniqueRecords.add(uniqueKey);
+                            data.push({
+                                name, title, email, phone, home
+                            });
+                            uniq_not_found = 0;
+                            total_indexed++;
+                        }
+                        else{
+                            uniq_not_found++;
+                            console.log('uniq_not_found try :  ',uniq_not_found);
+                            if(uniq_not_found > 500){
+                                console.log('uniq_not_found end ',uniq_not_found);
+                                continue_search = false;
+                                break;
+                            }
+                        }
 
-                const uniqueKey = `${name}-${title}-${email}-${phone}-${home}`;
-                
-                if (!uniqueRecords.has(uniqueKey)) {
-                    uniqueRecords.add(uniqueKey);
-                    data.push({
-                        name, title, email, phone, home
-                    });
-                    uniq_not_found = 0;
-                    total_indexed++;
-                }
-                else{
-                    uniq_not_found++;
-                    if(uniq_not_found > 100){
-                        continue_search = false;
-                        break;
+                    } catch (error) {
+                        console.log('cannpot click row index trying again :', index);
+                        await page.waitForTimeout(1500);
                     }
                 }
             } catch (error) {
@@ -142,23 +168,12 @@ async function getDataFromPage(url, limit) {
             }
         }
         try {
-            // people-pane__container
-            if (total_rows > 5) {
-                await page.evaluate((total_rows) => {
-                    const fifthElement = document.querySelector('.gio-entry-list__item-wrapper:nth-child(5)');
-                    // const fifthElement = document.querySelector('.gio-entry-list__item-wrapper:nth-child('+total_rows+')');
-                    fifthElement?.scrollIntoView({ behavior: 'smooth', block: 'end' });
-                }, total_rows);
-            }
-            await page.waitForTimeout(5000);
-
-            await page.waitForTimeout(15000);
-
-            rows = await page.$$('.people-lists__list');
-            total_rows = rows.length;
-            console.log('part rows : ', total_rows);
-            console.log('//////////////////////all data', data);
-            console.log('////////////////////// end all data');
+            
+            await page.evaluate(() => {
+                const fifthElement = document.querySelector('.gio-entry-list__item-wrapper:last-child'); 
+                fifthElement?.scrollIntoView({ behavior: 'smooth', block: 'end' });
+            });
+           
             if (total_rows <= 0) {
                 console.log('exit due to total rows is ' + total_rows);
                 break;
@@ -169,7 +184,8 @@ async function getDataFromPage(url, limit) {
         }
 
     }
-
+    console.log(total_indexed , totalNumber, continue_search,iterated_data,totalNumber);
+    console.log(total_indexed < totalNumber,continue_search,iterated_data<totalNumber);
     console.log(`Total unique indexed rows: ${data.length}`);
 
     console.log(`Total indexed rows: ${total_indexed}`);
@@ -180,11 +196,14 @@ async function getDataFromPage(url, limit) {
 
 app.get('/demand_base', async (req, res) => {
     try {
+        console.time("Execution Time");
         let limit = parseInt(req.query.limit) || 5;
-        const url = `https://authentication.demandbase.com/signin`;
+        let url = req.query.url || '';
+        // const url = `https://authentication.demandbase.com/signin`;
         const data = await getDataFromPage(url, limit);
         console.log(`Received request for : with limit: ${limit}`);
 
+        console.timeEnd("Execution Time");
         return res.json(data);
     } catch (error) {
         console.error(`Error in /scrap route: ${error.message}`);
